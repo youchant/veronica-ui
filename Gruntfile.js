562525a2724f11094b24c95e6b2470140306db0f
gochant <guochant@gmail.com>
2016-08-12 18:28:56 +0800

    change file structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,36 +3,26 @@
 
 module.exports = function (grunt) {
 
-    function simpleClone(oldObject) {
-        return JSON.parse(JSON.stringify(oldObject));
-    }
+    function createOptions() {
 
-    function createOptions(isPro) {
-
-        var moduleName = isPro ? './veronica-ui-pro' : './veronica-ui';
-        var endFile = isPro ? 'tools/wrap_pro.end' : 'tools/wrap.end';
+        var moduleName = './veronica-ui';
+        var endFile = 'tools/wrap.end';
+        var startFile = 'tools/wrap.start';
 
         var options = {
-            appDir: './lib',
-            baseUrl: '.',
+           // appDir: './lib',
+            baseUrl: './lib',
             dir: './dist',
             paths: {
                 "jquery": "empty:",
-                //'kendo-ui': '../bower_components/kendo-ui-core/src/js/kendo.ui.core',
                 'kendo-ui': '../assets/kendo-ui-core/dist/js/kendo.custom',
                 'kendo-ui-messages': '../assets/kendo-ui-core/dist/js/kendo.messages.zh-CN.min',
-                //'kendo-ui-pro': 'empty:',
-                //'kendo-ui-pro-culture': 'empty:',
-                //'kendo-ui-pro-messages': 'empty:',
-                'kendo-ui-pro': '../assets/kendo-ui-pro/js/kendo.web.min',
-                'kendo-ui-pro-culture': '../assets/kendo-ui-pro/js/cultures/kendo.culture.zh-CN.min',
-                'kendo-ui-pro-messages': '../assets/kendo-ui-pro/js/messages/kendo.messages.zh-CN.min',
                 'text': '../bower_components/requirejs-text/text',
                 'bootstrap': '../bower_components/bootstrap/dist/js/bootstrap',
                 'bootstrap-datetimepicker': '../bower_components/smalot-bootstrap-datetimepicker/js/bootstrap-datetimepicker',
                 'bootstrap-datetimepicker-cn': '../bower_components/smalot-bootstrap-datetimepicker/js/locales/bootstrap-datetimepicker.zh-CN',
                 'bs-datetimepicker': '../bower_components/bs-datetimepicker/src/js/bootstrap-datetimepicker',
-                'bootstrap-paginator': '../bower_components/bootstrap-paginator/src/bootstrap-paginator',
+                //'bootstrap-paginator': '../bower_components/bootstrap-paginator/src/bootstrap-paginator',
                 'jquery-form': '../bower_components/jquery-form/jquery.form',
                 'jquery-validation': '../bower_components/jquery-validation/dist/jquery.validate',
                 'jquery-validation-unobtrusive': '../bower_components/jquery-validation-unobtrusive/jquery.validate.unobtrusive',
@@ -65,7 +55,7 @@ module.exports = function (grunt) {
                 exclude: ["jquery", "text"]
             }],
             "wrap": {
-                "startFile": "tools/wrap.start",
+                "startFile": startFile,
                 "endFile": endFile
             },
             removeCombined: false,
@@ -83,23 +73,19 @@ module.exports = function (grunt) {
         pkg: grunt.file.readJSON('package.json'),
         requirejs: {
             main: {
-                options: createOptions(false)
+                options: createOptions()
             }
         },
         clean: {
             main: [
-            'dist/build.txt',
-            'dist/text.js',
-            'dist/*.js',
-            '!dist/veronica-ui.*',
-            '!dist/veronica-ui-pro.*'
+            'dist/**/*',
+            '!dist/veronica-ui.*'
             ]
         },
         uglify: {
             main: {
                 files: {
-                    'dist/veronica-ui.min.js': ['dist/veronica-ui.js'],
-                    'dist/veronica-ui-pro.min.js': ['dist/veronica-ui-pro.js']
+                    'dist/veronica-ui.min.js': ['dist/veronica-ui.js']
                 },
                 report: 'gzip'
             }
