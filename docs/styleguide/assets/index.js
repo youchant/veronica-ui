3ff7393289da1a900d436a2b65b10a94de5c76fd
gochant <guochant@gmail.com>
2016-10-20 18:24:57 +0800

    clear up

--- docs/styleguide/assets/index.js
+++
@@ -1,8 +0,0 @@
-; (function ($, window, document, undefined) {
-
-    "use strict";
-    $(function() {
-        $('.menu-vertical').submenu();
-    });
-
-})(jQuery, window, document);
--- src/js/veronica-ui.js
+++
@@ -1,9 +0,0 @@
-define([
-    './veronicaExt/appExt/_combine',
-    './veronicaExt/viewExt/_combine'
-], function () {
-    var args = Array.prototype.slice.call(arguments);
-    return function (app) {
-        app.use(args);
-    }
-});
