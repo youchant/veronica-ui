146733ecc7d512d0f871e573cb367da83278e11a
gochant <guochant@gmail.com>
2016-11-07 17:16:39 +0800

    update keboacy

--- dist/js/keboacy.js
+++ dist/js/keboacy.js
@@ -1,4 +1,4 @@
-/*! keboacy - v0.1.0 - 2016-10-26 */
+/*! keboacy - v0.1.0 - 2016-11-07 */
 (function (f, define) {
     define(['jquery', 'kendo-ui'], f);
 })(function ($, kendo) {
@@ -498,7 +498,8 @@
     var File = Widget.extend({
         options: {
             name: 'File',
-            label: 'Browse'
+            label: 'Browse',
+            compact: false
         },
         init: function (element, options) {
             var me = this;
@@ -510,6 +511,7 @@
         _wrapper: function(){
             var me = this;
             var element = me.element;
+            var options = me.options;
             var elementDom = element[0];
             var wrapper;
 
@@ -523,9 +525,18 @@
                 '<input type="text" class="form-control" readonly>' +
                 '</div>';
 
+            if(this.options.compact){
+                parent = '<div class="btn-group"><button class="btn btn-default fileinput-button">' +
+                    me.options.label + '&hellip;' +
+                    '</button></div>'
+            }
+
             me.wrapper = $(parent).addClass('k-widget k-file').addClass(elementDom.className);
             element.replaceWith(me.wrapper);
             me.wrapper.find('.btn').append(element);
+
+            // 修复 replaceWith 缺失的 data
+            element.data("kendo" + options.prefix + options.name, me);
         },
         _bindEvents: function(){
             var me = this;
@@ -533,6 +544,9 @@
                 var files = $(e.target)[0].files;
                 var name = files.length === 0 ? '' : files[0].name;
                 me.wrapper.find('input:text').val(name);
+                me.trigger('change', {
+                    originalEvent: e
+                });
             });
         }
     });
